53106beb482b74bc3c3da407e5ec2ec4df7331b1
Zack Jameson <zacharymjameson@gmail.com>
2021-01-10 15:34:15 -0500

    initial commit

---
+++ datastore.js
@@ -0,0 +1,67 @@
+const STORE = [
+      {
+        question: 'Where was Gondor when the Westfold fell?',
+        answers: [
+          'Cair Andros',
+          'Osgiliath',
+          'Ravenhill',
+          'Dunland',
+          'Angmar'
+          ],
+        correctAnswer: 'Osgiliath' || 'Cair Andros',
+      },
+      {
+        question: 'One does not simply walk into which realm?',
+        answers: [
+          'Mordor',
+          'Rohan',
+          'Hobbiton',
+          'Gondor',
+          'Iron Hills'
+        ],
+        correctAnswer: 'Mordor',
+      },
+      {
+        question: 'Gimli won the battle of killing orcs by what score at Helm\'s Deep?',
+        answers: [
+          '43-42',
+          '37-16',
+          '28-3',
+          'It was a tie',
+          'Gimlie actually lost'
+        ],
+        correctAnswer: '43-42',
+      },
+      {
+        question: 'In Fangorn forest, what do Merry and Pippen drink in order to inexplicably grow taller?',
+        answers: [
+          'Water',
+          'Mead',
+          'Ent-draught',
+          'Ale',
+          'Orc-draught'
+        ],
+        correctAnswer: 'Ent-draught',
+      },
+      {
+        question: 'What is the password which need be spoken in order to enter the Mines of Moria through the Doors of Durin?',
+        answers: [
+          'Open Sesame',
+          'Annon Edhellen, edro hi ammen!',
+          'Fennas Nogothrim, lasto beth lammen',
+          'Mellon',
+          'Ando Eldarinwa…a lasta quettanya, Fenda Casarinwa'
+        ],
+        correctAnswer: 'Mellon',
+      },
+      {
+        question: 'Viggo Mortensen (Aragorn) actually breaks his toe after kicking the helmet of a fallen Uruk-hai after believing what to be true?',
+        answers: [
+          'They had run out of Elvish bread',
+          'His reservation at the inn of the Prancing Pony was mistakenly cancelled',
+          'He believed to have failed Merry and Pippen',
+          'Legolas had led them down the wrong path in pursuit of Merry and Pippen across the Gap of Rohan'
+        ],
+        correctAnswer: 'He believed to have failed Merry and Pippen',
+      }
+];
